a28a9b7ee4ccd3773e224edca6a57c47dbe86a83
Felipe Zani <fzani@msn.com>
2024-07-23 16:59:47 +0100

    WIP | Review project rules
    Review (add missing rule):
     A security is a financial instrument identified by an ISIN, an alphanumeric code of 12 characters.

--- SecuritiesPriceService/Models/Security.cs
+++ SecuritiesPriceService/Models/Security.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BNP.SecuritiesPriceService.Models;
 
 public class Security
 {
     public int Id { get; set; }
+
+    [StringLength(12)]
     public string ISIN { get; set; }
+
     public decimal Price { get; set; }
 }
+
